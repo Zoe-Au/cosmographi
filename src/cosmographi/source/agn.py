90df7306d67b67a343e57bbc48c5a01978e33345
Zoe-Au <zaugspach@gmail.com>
2026-06-16 20:02:23 -0400

    fixed space

--- src/cosmographi/source/agn.py
+++ src/cosmographi/source/agn.py
@@ -186,12 +186,12 @@ class AGNSourceTong2026(TransientSource):
     accretion_rate: Param
 
     def __init__(self, cosmology: Cosmology = None, name: str = None, blackhole_mass: float = None, 
-                 edd_ratio: float = None, **kwargs) -> None:
+                 accretion_rate: float = None, **kwargs) -> None:
         super().__init__(cosmology=cosmology, name=name, **kwargs)
         self.blackhole_mass = Param("blackhole_mass", blackhole_mass, shape=(), 
                                    description="Mass of the black hole", units="solar masses")
         self.accretion_rate = Param("accretion_rate", accretion_rate, shape=(), 
-                                   description="Eddington ratio of the black hole", units="dimensionless")  
+                                   description="accretion_rate of the black hole", units="dimensionless")  
         
     @forward
     def luminosity_density(self, start: float, end: float, z: float) -> None:
