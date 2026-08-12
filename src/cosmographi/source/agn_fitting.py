578bfe6fc5d03ff8e8b8c9a6035ec61617d2f450
Zoe-Au <zaugspach@gmail.com>
2026-08-12 09:32:14 -0400

    draft update, may be buggy, waiting for run

--- src/cosmographi/source/agn_fitting.py
+++ src/cosmographi/source/agn_fitting.py
@@ -80,22 +80,20 @@ class PowerLawPrior_logpdf(ck.Module):
 #TODO: don't hardcode effects
 #TODO: look over  signatures
 class LogLikelihoodAGN(ck.Module):
-    def __init__(self, y, y_err, times, name=None, z=None, instrument: Instrument=None, band: jnp.ndarray=None, exp_time: jnp.ndarray=None, sky_brightness: jnp.ndarray=None, PSF_Aeff: jnp.ndarray=None, blackhole_mass=None, accretion_rate=None, inclination_angle=None, rin_to_rng=None, perturbations=None, luminosity_density_scaling=None, A_V_c00mw=None, R_V_c00mw=None, **kwargs):
+    def __init__(self, y, y_err, start_time, end_time, obs_t, name=None, z=None, instrument: Instrument=None, band: jnp.ndarray=None, exp_time: jnp.ndarray=None, sky_brightness: jnp.ndarray=None, PSF_Aeff: jnp.ndarray=None, blackhole_mass=None, accretion_rate=None, inclination_angle=None, rin_to_rng=None, perturbations=None, luminosity_density_scaling=None, A_V_c00mw=None, R_V_c00mw=None, **kwargs):
+        """times are rest-frame times for which the original source's perturbations are defined
+        """
         # TODO: leave all of these as parameters? -> not for band,  exp_time,  sky_brightness, PSF_Aeff
         super().__init__(name)
         self.y = y
         self.y_err = y_err
-        self.times = times
+        self.obs_t = obs_t
         self.instrument = instrument
         self.band = band
         self.exp_time = exp_time
         self.sky_brightness = sky_brightness
         self.PSF_Aeff = PSF_Aeff
         c = Cosmology()
-        if times is not None:
-            start_time, end_time = times[0], times[-1] 
-        else:
-            start_time, end_time = None, None
         self.source = source_factory(AGNSourceLipunova2018, MWExtinction_Calzetti00)(cosmology=c, A_V_c00mw=A_V_c00mw, R_V_c00mw=R_V_c00mw, blackhole_mass=blackhole_mass, accretion_rate=accretion_rate, rin_to_rng=rin_to_rng, inclination_angle=inclination_angle, perturbations=perturbations, start_time=start_time, end_time=end_time,  luminosity_density_scaling=luminosity_density_scaling)
         self.source.z = ck.Param("z", z, description="the redshift of the AGN")
         self.source.t0 = 0 #TODO:??
@@ -105,14 +103,14 @@ class LogLikelihoodAGN(ck.Module):
     @ck.forward
     def __call__(self, key):
         band, exp_time, sky_brightness,PSF_Aeff = jnp.atleast_1d(self.band), jnp.atleast_1d(self.exp_time), jnp.atleast_1d(self.sky_brightness), jnp.atleast_1d(self.PSF_Aeff)
-        result = self.vobs(key, band, exp_time, sky_brightness, PSF_Aeff, self.source, self.times)[0] # TODO: is it supposed to be this one? bc there is randomness in the rror too
+        result = self.vobs(key, band, exp_time, sky_brightness, PSF_Aeff, self.source, self.obs_t)[0] # TODO: is it supposed to be this one? bc there is randomness in the rror too
         return - 0.5* jnp.sum(((self.y - result)/self.y_err)**2) # for annealing divide by 100-10-1 acept bad values unstuck, try for prior if it doesnt work, or posterior
 
 class LogPosterior(ck.Module):
-    def __init__(self, key, y, y_err, times, instrument, band: jnp.ndarray, exp_time: jnp.ndarray, sky_brightness: jnp.ndarray, PSF_Aeff: jnp.ndarray, temp=1, name: str=None, z: float=None, blackhole_mass: float=None, accretion_rate: float=None, inclination_angle: float=None, rin_to_rng: float=None, perturbations: jnp.ndarray=None, luminosity_density_scaling: float=None, A_V_c00mw: float=None, R_V_c00mw: float=None, alpha: float=None, A: float=None, **kwargs):
+    def __init__(self, key, y, y_err, start_time, end_time, obs_t, instrument, band: jnp.ndarray, exp_time: jnp.ndarray, sky_brightness: jnp.ndarray, PSF_Aeff: jnp.ndarray, temp=1, name: str=None, z: float=None, blackhole_mass: float=None, accretion_rate: float=None, inclination_angle: float=None, rin_to_rng: float=None, perturbations: jnp.ndarray=None, luminosity_density_scaling: float=None, A_V_c00mw: float=None, R_V_c00mw: float=None, alpha: float=None, A: float=None, **kwargs):
         # TODO: leave all of these as parameters?
         super().__init__(name)
-        self.log_likelihood = LogLikelihoodAGN(name= "log_likelihood", y=y, y_err=y_err, times=times, z=z, instrument=instrument, band=band, exp_time=exp_time, sky_brightness=sky_brightness, PSF_Aeff=PSF_Aeff, blackhole_mass=blackhole_mass, accretion_rate=accretion_rate, inclination_angle=inclination_angle, rin_to_rng=rin_to_rng, perturbations=perturbations, luminosity_density_scaling=luminosity_density_scaling, A_V_c00mw=A_V_c00mw, R_V_c00mw=R_V_c00mw) 
+        self.log_likelihood = LogLikelihoodAGN(name= "log_likelihood", y=y, y_err=y_err, obs_t=obs_t, end_time=end_time, start_time=start_time, z=z, instrument=instrument, band=band, exp_time=exp_time, sky_brightness=sky_brightness, PSF_Aeff=PSF_Aeff, blackhole_mass=blackhole_mass, accretion_rate=accretion_rate, inclination_angle=inclination_angle, rin_to_rng=rin_to_rng, perturbations=perturbations, luminosity_density_scaling=luminosity_density_scaling, A_V_c00mw=A_V_c00mw, R_V_c00mw=R_V_c00mw) 
         self.log_prior = PowerLawPrior_logpdf(name="log_prior", alpha=alpha, A=A, perturbations=self.log_likelihood.source.perturbations)
         self.key = key
         self.temp = temp # for annealing
@@ -168,7 +166,6 @@ class AGNFit(ck.Module):
             keys = jax.random.split(key, n_samples)
             return jax.lax.scan(one_step, init_state, keys)
 
-
         temperatures = [100, 30, 10, 3, 1]
 
         for temp in temperatures:
@@ -188,119 +185,55 @@ class AGNFit(ck.Module):
         # plot first and last states to see if we are exploring
         return d
 
-    def plot(self, true_values: dict[str, any]=None):
-        """True values is a dict where param names are keys"""
-        idata = az.from_dict(self.fit())
-        axes = az.plot_trace(idata)
+    def plot(self, true_values: dict[str, any] = None):
+        """True values is a dict where param names are keys.
+        `array_param` is the one vector-valued parameter; its true values are
+        shown in the posterior and corner plots but not the trace plot."""
+        d = self.fit()
+        idata = az.from_dict(d)
+        var_names = list(idata.posterior.data_vars)
+
+        lines = None
         if true_values is not None:
-            for i, var in enumerate(idata.posterior.data_vars):
-                name = str(var)
-                if name in true_values:
-                    value = np.asarray(true_values[name])
-                    if value.ndim == 0:
-                        axes[i, 0].axvline(value, color="red", linestyle="--")
-                        axes[i, 1].axhline(value, color="red", linestyle="--")
+            lines = [(name, {}, true_values[name]) for name in var_names if name in true_values and name != "perturbations"]
+        az.plot_trace(idata, lines=lines)
         plt.show()
-        posterior_axes = az.plot_posterior(idata)
-        axes = list(np.asarray(posterior_axes, dtype=object).flat)
+
+        ref_val = None
         if true_values is not None:
-            axis_idx = 0
-            for var in idata.posterior.data_vars:
-                name = str(var)
-                shape = idata.posterior[name].shape[2:]
-                n_axes = int(np.prod(shape)) if shape else 1
-
-                if name in true_values:
-                    value = np.asarray(true_values[name])
-                    if value.ndim == 0:
-                        axes[axis_idx].axvline(value, color="red", linestyle="--")
-                    else:
-                        for j, v in enumerate(value.ravel()):
-                            axes[axis_idx + j].axvline(v, color="red", linestyle="--")
-
-                axis_idx += n_axes
-
+            ref_val = []
+            for name in var_names:
+                if name == "perturbations" and name in true_values:
+                    ref_val.extend(np.asarray(true_values[name]).ravel())
+                elif name in true_values:
+                    ref_val.append(true_values[name])
+                else:
+                    ref_val.append(None)
+        az.plot_posterior(idata, ref_val=ref_val)
         plt.show()
 
-        # Pair plot
-        az.plot_pair(idata, kind="kde", marginals=True)
-
-        plt.show()
-
-
-# class AGNFit(ck.Module):
-#     def __init__(self, key, y, y_err, name=None, **kwargs):
-#         super().__init__(name)
-#         key, key2 = jax.random.split(key)
-#         self.log_posterior = LogPosterior(name="log_posterior", key=key, y=y, y_err=y_err, **kwargs)
-#         self.key = key2
-
-#     def fit(self):
-#         j_logposterior = jax.jit(self.log_posterior) # use everywhere so we compile it only once
-#         n_chains = 4
-#         n_samples = 5000
-#         key, init_key, sample_key = jax.random.split(self.key, 3)
-#         init_keys = jax.random.split(init_key, n_chains)
-
-#         def get_initial_position(base, key):
-#             return 0.01 * jax.random.normal(key, shape=jnp.shape(base)) * base + base
-#             # return base # starting t true value
-
-#         initial_positions = jax.vmap(get_initial_position, in_axes=(None, 0))(self.log_posterior.get_values(), init_keys)
-#         warmup = bx.window_adaptation(bx.hmc, j_logposterior, num_integration_steps=12, target_acceptance_rate=0.8) #TODO : tune num_integration_steps and num warmup steps
-#         print("caling warmup.run")
-#         # adaptation_results, adaptation_info = jax.vmap(warmup.run, in_axes=(None, 0))(key, initial_position) 
-#         # check step size, check that it is actually accepting samples ~80%
-#         # check acceptance rate for real run
-#         # annealing 
-#         adaptation_results, adaptation_info = warmup.run(key, initial_positions[0]) 
-#         print('calling warmup complete')
-#         chain_keys = jax.random.split(sample_key, n_chains)
-#         kernel = bx.hmc(jax.jit(j_logposterior), **adaptation_results.parameters).step
-
-#         def turn_position_to_state(position, j_logposterior):
-#             return bx.hmc.init(position, j_logposterior)
-        
-#         init_states = jax.vmap(turn_position_to_state, in_axes=(0, None))(initial_positions, j_logposterior)
-
-#         @jax.jit
-#         def one_step(init_states, keys):
-#             states, _ = kernel(keys, init_states)
-#             return states, states
-        
-#         @jax.jit
-#         def run_chain(init_state, key):
-#             keys = jax.random.split(key, n_samples)
-#             return jax.lax.scan(one_step, init_state, keys)
-
-#         _, states = jax.vmap(run_chain)(init_states, chain_keys)
-#         print("sampling done")
-#         d = {}
-#         for param in j_logposterior.dynamic_params:
-#             idx = self.log_posterior.find_index(param)
-#             d[param.name] = states.position[:, :, idx]
-#         # plot first and last states to see if we are exploring
-#         return d
-
-#     def plot(self, true_values=None):
-#         """True values is a dict where param names are keys"""
-#         idata = az.from_dict(self.fit())
-#         axes = az.plot_trace(idata)
-#         if true_values is not None:
-#             for i, var in enumerate(idata.posterior.data_vars):
-#                 name = str(var)
-#                 if name in true_values:
-#                     axes[i, 0].axvline(true_values[name], color="red", linestyle="--")
-#                     axes[i, 1].axhline(true_values[name], color="red", linestyle="--")
-#         plt.show()
-#         axes = np.atleast_1d(az.plot_posterior(idata)).ravel()
-#         if true_values is not None:
-#             for i, var in enumerate(idata.posterior.data_vars):
-#                 name = str(var)
-#                 if name in true_values:
-#                     axes[i].axvline(true_values[name], color="red", linestyle="--")
-#         az.plot_pair(idata, kind="kde", marginals=True)
-
-    
-        
-            
+        pair_idata = idata.copy()
+        pair_var_names = []
+        pair_reference_values = {}
+        for name in var_names:
+            if name != "perturbations":
+                pair_var_names.append(name)
+                if true_values is not None and name in true_values:
+                    pair_reference_values[name] = true_values[name]
+            else:
+                data = idata.posterior["perturbations"]
+                n_elements = data.sizes["samples"]
+                true_array = None
+                if true_values is not None and "perturbations" in true_values:
+                    true_array = np.asarray(true_values["perturbations"]).ravel()
+                for i in range(n_elements):
+                    scalar_name = f"perturbations[{i}]"
+                    pair_idata.posterior[scalar_name] = (data.isel(samples=i) .drop_vars("samples", errors="ignore"))
+                    pair_var_names.append(scalar_name)
+                    if true_array is not None:
+                        pair_reference_values[scalar_name] = true_array[i]
+            az.plot_pair(pair_idata, var_names=pair_var_names, kind="kde", marginals=True,
+                reference_values=(pair_reference_values if true_values is not None else None),
+                reference_values_kwargs={"color": "red", "marker": "o"})
+            plt.show()
+        return d
